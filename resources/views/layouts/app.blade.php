1d3132a13c6e213d425073580a5a2e710b958e1e
gedwu <gedwidas@gmail.com>
2019-02-14 12:03:12 +0200

    First commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        <title>BitDegree Task</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
+    </body>
+</html>
