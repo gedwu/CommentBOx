b2de7f3c2bbaf550294473192cffffd66f5166a7
gedwu <gedwidas@gmail.com>
2019-02-15 09:09:56 +0200

    Fixed bug with user_id

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="user-id" content="{{ Auth::user()->id }}">
-
+    @auth
+        <meta name="user-id" content="{{ Auth::user()->id }}">
+    @endauth
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
